87ca0189b94f3eb02815574cc8364af287156c59
anthonyfdunphy <anthonyfdunphy@gmail.com>
2022-05-29 23:52:16 +0000

    attempt to chance js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,15 @@
+/*event listener added to check for window resizing*/
 window.addEventListener('resize', animateWindow);
 
+/*function to create visibility on resizing of window*/
 function animateWindow(){
     var x = document.getElementById("resize-text");
     x.style.visibility = "visible";
+    return true;
 }
 
+function animateFunction(){
+    if (animateWindow()){
+        console.log("Print To Console");
+    }
+}
