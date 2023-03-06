be57df20084dcee07897f277b8e0f6f96e432852
Anthony Dunphy <93882580+anthonyfdunphy@users.noreply.github.com>
2023-03-06 23:26:48 +0000

    scroll added and styling added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,4 +9,36 @@ const scroll = new LocomotiveScroll({
     }
 });
 
+function customEase(t) {
+    if (t < 0.5) {
+      return 1 - Math.pow(2, -10 * t * 2);
+    } else {
+      return 0.5 - 0.5 * Math.pow(2, 10 * (t - 0.5));
+    }
+}
+  
+
+const textTop = document.getElementById('text-top');
+let currentValue = 0;
+
+scroll.on('scroll', function(scroll) {
+  const scrollY = scroll.y;
+  const matrixValue = textTop.style.transform;
+  const regex = /-?\d+\.\d+/;
+  const match = matrixValue.match(regex);
+  const extractedValue = match ? Number(match[0]) : 0;
+
+  if (extractedValue !== currentValue) {
+    currentValue = extractedValue;
+    const newValue = (currentValue + 100) / 100; // convert range to 0-1
+    textTop.style.opacity = newValue.toFixed(2); // set opacity to the new value
+    console.log(newValue);
+  }
+});
+
+
+
+
+
+
 console.log("working!")
