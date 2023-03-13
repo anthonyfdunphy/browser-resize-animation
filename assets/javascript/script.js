04c0f4341fa730a8efa2a7ff63d9edd8221cebba
Anthony Dunphy <93882580+anthonyfdunphy@users.noreply.github.com>
2023-03-13 15:02:35 +0000

    menu working with vanilla js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -43,16 +43,54 @@ const liElements = document.querySelectorAll('.work li');
 const inDetail = document.querySelector('.in-detail');
 const exitDetail = document.querySelector('.exit');
 
+// this event listener checks when the + symbol is clicked and runs scripts
+
 linkElement.addEventListener('click', function() {
   liElements.forEach(function(liElement) {
-    liElement.style.height = '0px'; // Change the value as needed
+    liElement.style.height = '0%'; // Change the value as needed
   });
 
   ulElement.style.opacity = '0'; // Set the opacity of the ul element to 0
+
   ulElement.addEventListener('transitionend', function() {
-    inDetail.classList.remove('hidden');
+    // function to set height of detail div for timer
+    function setHeight() {
+      inDetail.style.height = "100%";
+      inDetail.style.opacity = "100%";
+    };
+
+    inDetail.classList.remove('hidden'); // Add the 'hidden' class from the detail div
     ulElement.classList.add('hidden'); // Add the 'hidden' class to the ul element when the CSS transition ends
+
+    const myTimeout = setTimeout(setHeight, 50);  // Set Height of detail div after 0.5 ms
+
+
   }, { once: true }); // Use the 'once' option to remove the event listener after it has fired
+});
+
+// this event listener checks when the X in the detail div is clicked and runs scripts
+exitDetail.addEventListener('click', function() {
+  console.log("exit detail function working!");
+  inDetail.style.height = "0%"; // Set height to 0 when closing
+
+  inDetail.addEventListener('transitionend', function() {
+    console.log("transition! exit detail function working!");
+    inDetail.style.opacity = "0";
+    liElements.forEach(function(liElement) {
+      liElement.style.height = "80px"; // Change the value as needed
+    });
+
+    ulElement.classList.remove('hidden'); //Remove the 'hidden' class to the ul element when the CSS transition ends
+
+    // function to set height of detail div for timer
+    function setOpacity() {
+      ulElement.style.opacity = '1';
+    };
+
+    const opacityTimer = setTimeout(setOpacity, 50);  // Set Height of detail div after 0.5 ms
+    
+
+  }, { once: true });
 });
 
 
